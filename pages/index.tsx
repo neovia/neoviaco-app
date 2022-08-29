c32166f9793f97dafca72f125916800bbcd747c8
IMAM Souleimane <souleimane@IMAMs-MacBook-Pro.local>
2022-08-29 11:30:24 +0200

    feature/github-pages - gh-pages test16

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,11 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome2 to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2 className={styles.title}>
+          Initialisation de Neovia.co app
+        </h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
