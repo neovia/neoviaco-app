2b34951dde88ed3fa7db29e7e584b54c0253442d
IMAM Souleimane <souleimane@IMAMs-MacBook-Pro.local>
2022-08-29 16:16:50 +0200

    develop - test gitactions

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <h2 className={styles.title}>
-          Initialisation de Neovia.co app test 2
+          Initialisation de Neovia.co app test 3 gitactions
         </h2>
 
         <p className={styles.description}>
